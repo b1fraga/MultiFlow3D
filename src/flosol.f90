48e956182b5855308ccdff0591bf8006d8a5084a
mcbarton <150042563+mcbarton@users.noreply.github.com>
2026-06-09 14:22:52 +0100

    Fix all linter issues + turn on all warnings for MPI_pt.f90

--- src/flosol.f90
+++ src/flosol.f90
@@ -6,6 +6,7 @@
           use multidata
           use vars_pt
           use, intrinsic :: iso_fortran_env, only: dp => real64
+          use multiflow3d_MPI_pt, only : MPI_pt
           implicit none
           real(dp)  :: wtimedum,wtime_total,wtime_solver,wtime_ib
           real(dp) :: wtime_cd,wtime_lpt
